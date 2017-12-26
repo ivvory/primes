a83bcdb15d32a3688ec027a19c4501bd56c25fbb
Ilia <ivv.evol@gmail.com>
2017-12-26 13:49:39 +0300

    add: mod plot

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ To generate csv file with prime frequency make the following steps:
 
 ## Research
 
-Four intervals were explored:
+Following intervals were explored:
 1. 2^0, 2^0 + 10^7
 2. 2^256, 2^256 + 10^7
 3. 2^512, 2^512 + 10^7
@@ -33,3 +33,11 @@ Four intervals were explored:
 ![pow_512](results/fig_pow_512.png)
 ![pow_1024](results/fig_pow_1024.png)
 ![pow_2048](results/fig_pow_2048.png)
+
+### A multiple of six
+
+I noticed a frequency of differences which are multiple of six is significantly more than others.
+The graph below shows two lines, one has only points that meet the condition `difference % 6 == 0`
+and the second line consists of points that do not.
+
+![mod_6_pow_512](results/mod_6_pow_512.png)
--- draw.py
+++ draw.py
@@ -6,12 +6,29 @@ def prime_count(filename):
     return pandas.read_csv(filename)['count'].sum() - 1
 
 
-_pow = 2048
-df1 = pandas.read_csv(f'results/pow_{_pow}.csv')
-df1.plot(x='difference', y='count')
+def plot_frequency(_pow):
+    df1 = pandas.read_csv(f'results/pow_{_pow}.csv')
+    df1.plot(x='difference', y='count')
 
-plt.xlabel('Difference between nearest primes')
-plt.ylabel('Count of differences')
-plt.title(f'Frequency of prime differences (2^{_pow}, 2^{_pow} + 10^7)')
+    plt.xlabel('Difference between nearest primes')
+    plt.ylabel('Count of differences')
+    plt.title(f'Frequency of prime differences (2^{_pow}, 2^{_pow} + 10^7)')
 
-plt.show()
+    plt.show()
+
+
+def plot_frequency_mod_6(_pow):
+    df = pandas.read_csv(f'results/pow_{_pow}.csv')
+    df1 = df.loc[df['difference'] % 6 == 0]
+    ax = df1.plot(x='difference', y='count', label='difference % 6 == 0')
+    df2 = df.loc[df['difference'] % 6 != 0]
+    df2.plot(x='difference', y='count', label='difference % 6 != 0', ax=ax)
+
+    plt.xlabel('Difference between nearest primes')
+    plt.ylabel('Count of differences')
+    plt.title(f'Frequency of prime differences mod 6 (2^{_pow}, 2^{_pow} + 10^7)')
+
+    plt.show()
+
+
+plot_frequency_mod_6(512)
